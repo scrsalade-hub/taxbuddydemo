20b121cc9e14aaf75cf4ca375f54ecdc547b16a7
Abdur-Rahim Abdus-Salam <scrsalade@gmail.clom>
2026-04-16 08:07:42 -0400

    api br

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 'taxbuddydemo.vercel.app',
+    port: 5173,
     proxy: {
       '/api': {
         target: 'https://taxbuddydemo.vercel.app',
